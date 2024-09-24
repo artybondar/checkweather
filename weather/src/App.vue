fc17fcee9dda68d9616c5b9b342a479d388e00fa
artybondar <bondaraf@gmail.com>
2024-09-24 16:35:57 +0300

    Заготовка сайта для проверки погоды по городам

---
+++ weather/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="weather">
+    <div class="container">
+
+      <div class="card weather-form">
+        <input type="text" class="weather-form__input" placeholder="введите город">
+        <button class="weather-form__btn">Показать</button>
+      </div>
+
+      <div class="weather-info">
+        <div class="weather-info__text">
+          <p class="card"></p>
+          <p class="card"></p>
+          <p class="card"></p>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="weather-bg">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
